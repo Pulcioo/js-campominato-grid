0fbe703024216a2659d135336abc5054dc345511
Davide Pulci <davide.pulci@gmail.com>
2022-03-15 14:54:39 +0100

    creata griglia con relative celle

--- script.js
+++ script.js
@@ -2,3 +2,26 @@ console.log("JS OK")
 
 // Creare una griglia di gioco quadrata, in cui ogni cella contiene un numero tra quelli compresi in un range compreso tra 1 e 100
 // Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.
+
+
+/////// 1.Creo una griglia di gioco su CSS
+
+/////// 2.Aggiungo il div con #griglia nel documento HTML
+
+/////// 3.Imposto la griglia su 10 righe e 10 colonne in JS
+
+// seleziono 'griglia' tramite ID
+const griglia = document.getElementById('griglia');
+// variabili d'appoggio per definire righe e colonne
+let colonne = 10
+let righe = 10
+// calcolo le celle totali
+let celleTotali = colonne * righe
+
+// 4.CICLO per ogni cella della griglia
+for (let i = 0; i < celleTotali; i++) {
+    // creo cella
+    const cella = document.createElement('div');
+    cella.classList.add('cella');
+    griglia.appendChild(cella)
+}
